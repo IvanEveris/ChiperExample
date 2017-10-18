2d346c4f5b7eabf914d80844e8b0b636790bfe9e
Ivan Pico Martin <ivan.pico.martin.sa@everis.com>
2017-10-18 10:20:24 +0200

    String Encripted

--- app/src/main/java/com/criptofields/example/MainActivity.java
+++ app/src/main/java/com/criptofields/example/MainActivity.java
@@ -3,8 +3,13 @@ package com.criptofields.example;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.security.KeyPair;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -14,21 +19,46 @@ import javax.crypto.SecretKey;
 
 public class MainActivity extends AppCompatActivity {
 
+    private KeyPair rsaKey;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         String inputText = "Password";
         Log.d("TAGTAG", "Input text " + inputText);
+
 
         String encryptedText = sha(inputText);
         Log.d("TAGTAG", "Output text " + encryptedText);
 
+        // GET KEY (GENERATE OR GET FROM CERTIFICATE
+        this.rsaKey = RSAEncryptDecrypt.generateRSAKey();
 
+        //create an inputstream from a string
+        try {
+            ByteArrayInputStream plainTextInputStream =
+                    new ByteArrayInputStream(inputText.getBytes("UTF-8"));
+
+            //encrypt the combined keys using rsa and store the encrypted value
+            byte[] encryptedAESKey = RSAEncryptDecrypt.encryptRSA(
+                    inputText.getBytes(Charset.forName("UTF-8")),
+                    this.rsaKey.getPublic());
+
+
+            //set ui textview to encrypted base64 encoded value
+            String encryptedString = new String(Base64.encode(encryptedAESKey, 0));
+
+
+            Log.d("TAGTAG", "Output text encryptedString " + encryptedString);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 
     }
+
     public static String sha(String string) {
         if (TextUtils.isEmpty(string)) {
             return "";
@@ -36,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         MessageDigest md5 = null;
         try {
             md5 = MessageDigest.getInstance("sha-256");
-            byte[] bytes = md5.digest((string ).getBytes());
+            byte[] bytes = md5.digest((string).getBytes());
             String result = "";
             for (byte b : bytes) {
                 String temp = Integer.toHexString(b & 0xff);
@@ -53,10 +83,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    public String encryption(String strNormalText){
+    public String encryption(String strNormalText) {
         String seedValue = "YourSecKey";
-        String normalTextEnc="";
+        String normalTextEnc = "";
         try {
             normalTextEnc = AESHelper.encrypt(seedValue, strNormalText);
         } catch (Exception e) {
@@ -64,9 +93,10 @@ public class MainActivity extends AppCompatActivity {
         }
         return normalTextEnc;
     }
-    public String decryption(String strEncryptedText){
+
+    public String decryption(String strEncryptedText) {
         String seedValue = "YourSecKey";
-        String strDecryptedText="";
+        String strDecryptedText = "";
         try {
             strDecryptedText = AESHelper.decrypt(seedValue, strEncryptedText);
         } catch (Exception e) {
